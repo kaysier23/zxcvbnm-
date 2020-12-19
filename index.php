80053d52b72550b5541bae9b5eff474e137b7dce
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 19:20:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-
 <html>
-<head>
-  <meta charset="UTF-8">
-  <title>
-  <link rel="stylesheet" href="./style.css">
+  <body>	
+		<div class = "header"> </div>
+			<div class = "navi">
+			<a href = "index.php"><font color = "black"> HOME</font></a>
+			<a href = "css.php"><font color = "black"> CSS</font></a>
+			<a href = "svg.php"><font color = "black"> SVG</font></a>
+			<a href = "canvas.php"><font color = "black"> CANVAS</font></a>
+			<a href = "aboutme.php"><font color = "black"> ABOUT ME</font></a>
+			</div>
+			<br>
+			<div class = "body2">
+           <p1><font size = "6">ABOUT ME</font></p1>
+<br>
+        <br>
+          <p><font size = "4">Hello I'm Kaysier Reno T. De los Reyes from BSIT-3A</font></p>
+        <p><font size = "4">This is my portfolio for ITL 303</font></p>
+        <p><font size = "4">I'm not very good in doing animations and designing things but I take my time to learn and I always practice when I have a free time.</font></p>
 
-</head>
-<body>
-<!-- partial:index.partial.html -->
-<div class="wrapper">
+        <div class="wrapper">
   <div class="candles">
     <div class="light__wave"></div>
     <div class="candle1">
@@ -44,10 +53,27 @@
   </div>
   <div class="floor">
   </div>
-  
-
 </div>
-<!-- partial -->
-  
-</body>
+			</div>
+			<div class = "footer"> 
+        <h3>
+			<a class="insta-footer" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">
+<div class="insta">
+  <div class="shape"></div>
+</div></a>
+<div class="fb">
+  <div class="facebook fbpos"><a class="fbanchor" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">F</a></div>
+</div>
+<div class="tw">
+  <div class="twitter twpos"><a class="twanchor" href="https://www.facebook.com/pages/Le-Vape-Cafe/585242891871155">T</a></div>
+</div>
+  </h3>
+  <div class = "TM">
+ 			<p><font size = "1"> Portfolio </font></p>
+			<p><font size = "1">© 2020 / kyzier.LTD / All rights reserved. </font></p>
+    </div>
+  </footer>
+			</div>
+		
+	</body>
 </html>

62606bf00d40d022af2dba9c4dc3731966d429ee
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-12-19 19:18:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,121 +1,53 @@
+
 <html>
-	<link rel="stylesheet" type = "text/css" href = "style.css"/>
+<head>
+  <meta charset="UTF-8">
+  <title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
 <body>
- <div class="row cf"> 
-          <div class="six col">
-        <a href="index.php" class="button" id="button-8">HOME</a>
+<!-- partial:index.partial.html -->
+<div class="wrapper">
+  <div class="candles">
+    <div class="light__wave"></div>
+    <div class="candle1">
+      <div class="candle1__body">
+        <div class="candle1__eyes">
+          <span class="candle1__eyes-one"></span>
+          <span class="candle1__eyes-two"></span>
+        </div>
+        <div class="candle1__mouth"></div>
       </div>
+      <div class="candle1__stick"></div>
+    </div>
+    
+    <div class="candle2">
+      <div class="candle2__body">
+        <div class="candle2__eyes">
+          <div class="candle2__eyes-one"></div>
+          <div class="candle2__eyes-two"></div>
+        </div>
       </div>
-<canvas></canvas>
-<Script>
-const canvas = document.querySelector('canvas')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-const c = canvas.getContext('2d')
+      <div class="candle2__stick"></div>
+    </div>
+    <div class="candle2__fire"></div>
+    <div class="sparkles-one"></div>
+    <div class="sparkles-two"></div>
+    <div class="candle__smoke-one">
 
-let mousex = undefined
-let mousey = undefined
+    </div>
+    <div class="candle__smoke-two">
 
-const circlesCount = 100
-const maxRadius = 140
-const circleArray = []
- 
-const emojiArray = [
-  '🤣',
-  '😇',
-  '💩',
-  '🦄',
-  '⛄️'
-]
-
-const debounce = (func) => {
-  let timer
-  return (event) => {
-    if (timer) { clearTimeout(timer) }
-    timer = setTimeout(func, 100, event)
-  }
-}
-
-window.addEventListener('resize', debounce(() => {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  
-  init()
-}))
-
-const init = () => {
-  circleArray.length = 0
-  for (let i = 0; i < circlesCount; i++) {
-    const radius = Math.random() * 30 + 10
-    const x = Math.random() * (innerWidth - radius  * 2) + radius
-    const y = Math.random() * (innerHeight - radius  * 2) + radius
-    const dx = (Math.random() - 0.5) * 2
-    const dy = (Math.random() - 0.5) * 2
-
-    circleArray.push(new Circle(x, y, dx, dy, radius))
-  }
-
-}
-
-const Circle = function(x, y, dx, dy, radius) {
-  this.x = x
-  this.y = y
-  this.dx = dx
-  this.dy = dy
-  this.radius = radius
-  this.minRadius = radius
-  this.text = emojiArray[Math.floor(Math.random() * emojiArray.length)]
-  
-  this.draw = function() {
-    c.font = `${this.radius}px Courier New`
-    c.fillText(this.text, this.x, this.y)
-  }
-  
-  this.update = function() {
-    if (this.x + this.radius > innerWidth || this.x < 0) {
-      this.dx = -this.dx
-    }
-
-    if (this.y  > innerHeight || this.y - this.radius < 0) {
-      this.dy = -this.dy
-    }
-
-    this.x += this.dx
-    this.y += this.dy
+    </div>
     
-    //interactivity
-    if (mousex - this.x < 50 && mousex - this.x > -50 && mousey - this.y < 50 && mousey - this.y > -50) {
-      if (this.radius < maxRadius) {
-        this.radius += 1
-        c.font = `${this.radius}px Courier New`
-      }
-    } else {
-      if (this.radius > this.minRadius) {
-        this.radius -= 1 
-        c.font = `${this.radius}px Courier New`
-      }
-    }
-    
-    this.draw()
-  }
-}
+  </div>
+  <div class="floor">
+  </div>
+  
 
-const animate = () => {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, innerWidth, innerHeight)
-
-  for (let i = 0; i < circleArray.length; i++) {
-    circleArray[i].update() 
-  }
-}
-
-init()
-animate()
-
-window.addEventListener('mousemove', (e) => {
-  mousex = e.x
-  mousey = e.y
-})
-</script>
+</div>
+<!-- partial -->
+  
 </body>
 </html>
